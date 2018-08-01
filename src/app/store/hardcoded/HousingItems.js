66bb1692b20178f8eb9aefc00180b6e9dd30d393
Rodislav MOLDOVAN <rmoldovan@pictet-technologies.com>
2018-08-01 11:53:22 +0200

    started adding real markdown documents, find housing, renting best practices

--- src/app/store/hardcoded/HousingItems.js
+++ src/app/store/hardcoded/HousingItems.js
@@ -8,17 +8,17 @@ export const housingItems: CategoryItem[] = [
         '/images/housing/find-online.jpg',
         'Here the websites you can use to find housing for rent and buy. There are multiple resources, still most have same offers.',
         'Find housing online',
-        ++i, '/wiki/housing/test.md'),
+        ++i, '/wiki/housing/find-housing-online.md'),
 
     new CategoryItem('Renting best practices', '', new Date(),
         '/images/housing/rent-best-practices.jpg',
         "When you are looking for a place to live, there are couple of things you should consider. Here's our list.",
         'Renting best practices',
-        ++i, '/wiki/housing/test2.md'),
+        ++i, '/wiki/housing/renting-best-practices.md'),
 
     new CategoryItem('Move-in checklist', '', new Date(),
         '/images/housing/move-in-checklist.jpg',
         "Once you move in there are few things to consider, like subscriptions, insurance and so on.",
         'Move in checklist',
-        ++i, '/wiki/housing/test2.md'),
+        ++i, '/wiki/housing/move-in-checklist.md'),
 ];
