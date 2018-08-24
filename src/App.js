ac08886f0c9b0a9c98f12cd4946f5199ef4a2a3c
Rodislav MOLDOVAN <rmoldovan@pictet-technologies.com>
2018-08-24 14:13:33 +0200

    small refactor, created theme component, prepared for adding logic to theme things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import CategoriesMenu from "./app/screen/CategoriesMenu";
 import CategoryView from "./app/screen/CategoryView";
 import CategoryItemView from "./app/screen/CategoryItemView";
 import Logo from "./app/screen/component/Logo";
 import {Route} from "react-router-dom";
 import RouterHandler from "./app/helper/RouterHandler";
-import CategoryItemView2 from "./app/screen/component/BasicMarkup";
+import Theme from "./app/screen/theme/Theme";
 
 class App extends React.Component {
 
@@ -15,6 +14,7 @@ class App extends React.Component {
             <div>
                 <Route exact path='/:categoryId?/:categoryItemId?' component={RouterHandler}/>
 
+                <Route component={Theme} />
                 <Route component={Logo} />
                 <Route component={CategoriesMenu}/>
 
---
+++ src/app/screen/theme/Theme.jsx
@@ -0,0 +1,35 @@
+// @flow
+
+import * as React from "react";
+import './Theme.css';
+
+export default class Theme extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="be_plane"/>
+
+                <div className="be_theme-image-left-right"/>
+                <div className="be_theme-image-center"/>
+
+                <div className="be_theme-sun"/>
+
+                <div className="be_theme-balloon"/>
+
+                <div className="be_theme-cloud-big"/>
+                <div className="be_theme-cloud-big-2"/>
+                <div className="be_theme-cloud-medium"/>
+                <div className="be_theme-cloud-small"/>
+
+                <div className="be_univers"/>
+
+                <div className="be_star-small"/>
+                <div className="be_star-small-top-corner"/>
+                <div className="be_star-small-bottom-corner"/>
+                <div className="be_star-big"/>
+            </div>
+        );
+    }
+
+}
