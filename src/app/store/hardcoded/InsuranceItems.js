9db0cfb795ac7e841c359c9a2c6f0e87723dba5e
Rodislav MOLDOVAN <rodislav@gmail.com>
2018-09-08 20:34:20 +0200

    added insurance image

--- src/app/store/hardcoded/InsuranceItems.js
+++ src/app/store/hardcoded/InsuranceItems.js
@@ -4,6 +4,6 @@ import CategoryItem from "../../data/CategoryItem";
 export const insuranceItems: CategoryItem[] = [
     new CategoryItem('Insurance companies',
         'What insurance companies are present in the country. A brief description on what they offer.',
-        '',
+        '/images/insurance/insurances-companies.png',
         '/wiki/insurance/insurance-companies.bm'),
 ];
