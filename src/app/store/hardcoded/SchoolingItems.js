b38eabb069926e0b3386b8f13dbf6062c046fcf4
Rodislav MOLDOVAN <rodislav@gmail.com>
2018-09-08 21:11:17 +0200

    added better images for schooling

--- src/app/store/hardcoded/SchoolingItems.js
+++ src/app/store/hardcoded/SchoolingItems.js
@@ -4,27 +4,27 @@ import CategoryItem from "../../data/CategoryItem";
 export const schoolingItems: CategoryItem[] = [
     new CategoryItem('Public Schools',
         'How schooling system works in Luxembourg. Where to register kids. What are the opening hours. What are the costs.',
-        '/images/schooling/how-it-works.png',
+        '/images/schooling/public-schools.png',
         '/wiki/schooling/public-schools.bm'),
 
     new CategoryItem('Foyer Scolaire',
         'Child care. What is Foyer Scolaire / Maison Relais. How to pricing works.',
-        '/images/schooling/foyer-scholaire.jpg',
+        '/images/schooling/foyer-scolaire.png',
         '/wiki/schooling/foyer-scolaire.bm'),
 
     new CategoryItem('What languages are taught',
         "Depending on the age of tuition your kids will learn in multiple languages. Here's how it works and why this is amazing.",
-        '/images/schooling/school-languages.jpg',
+        '/images/schooling/taught-languages.png',
         '/wiki/schooling/what-languages-are-taught.bm'),
 
     new CategoryItem('European schools',
         "Here's the list of european schools. Differences and similarities with national curriculum. How to get in for free.",
-        '/images/schooling/european-school.jpg',
+        '/images/schooling/european-school.png',
         '/wiki/schooling/european-school.bm'),
 
     new CategoryItem('English schools',
         "Here's the list of english schools. Differences and similarities with national curriculum",
-        '/images/schooling/english-school.jpg',
+        '/images/schooling/english-school.png',
         '/wiki/schooling/english-school.bm'),
 
     // new CategoryItem('French schools',
