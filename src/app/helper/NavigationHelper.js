e5cd1ee726e387cf340764702d8f6b12bc3051dc
Rodislav MOLDOVAN <rmoldovan@pictet-technologies.com>
2018-11-06 16:25:13 +0100

    added meta changes integration

--- src/app/helper/NavigationHelper.js
+++ src/app/helper/NavigationHelper.js
@@ -34,7 +34,7 @@ class NavigationHelper {
                 categoryStore.findCategoryItem(categoryItemId).subscribe((categoryItem: CategoryItem) => {
                     if(categoryItem) {
                         categoryStore.setCurrentCategoryItem(categoryItem);
-                        this.setTitle(categoryItem.title);
+                        this.updateMeta(categoryItem.title);
                     } else {
                         this.category(component, category);
                     }
@@ -47,18 +47,54 @@ class NavigationHelper {
         component.props.history.push('/' + category.id);
         categoryStore.setCurrentCategory(category);
 
-        this.setTitle(category.name);
+        this.updateMeta(category.name);
     }
 
     categoryItem(component: React.Component<EmptyProps>, categoryItem: CategoryItem) {
         component.props.history.push('/' + categoryStore.category.id + "/" + categoryItem.id);
         categoryStore.setCurrentCategoryItem(categoryItem);
 
-        this.setTitle(categoryItem.title);
+        this.updateMeta(categoryItem.title);
     }
 
-    setTitle(text: string) {
+    updateMeta(text: string) {
         document.title = text + ", Becoming.lu";
+
+        let category = categoryStore.category;
+        let categoryItem = categoryStore.categoryItem;
+
+        let metas: [] = document.getElementsByTagName("META");
+
+        for (const meta of metas) {
+            try {
+                const metaName = meta.attributes.getNamedItem("property").nodeValue;
+
+                if(metaName === "og:title") {
+                    meta.setAttribute("content", text + ", promoted by Becoming.lu");
+                }
+
+                if(categoryItem) {
+                    if(metaName === "og:image") {
+                        meta.setAttribute("content", "https://becoming.lu" + categoryItem.image);
+                    }
+
+                    if(metaName === "og:url") {
+                        meta.setAttribute("content", "https://becoming.lu/" + category.id + "/" + categoryItem.id);
+                    }
+                } else if(category) {
+                    if(metaName === "og:image") {
+                        meta.setAttribute("content", "https://becoming.lu/images/becoming.png");
+                    }
+
+                    if(metaName === "og:url") {
+                        meta.setAttribute("content", "https://becoming.lu/" + category.id);
+                    }
+                }
+            } catch (e) {
+                // ignore
+            }
+
+        }
     }
 }
 
