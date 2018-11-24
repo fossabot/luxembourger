b15a145d72600b66d086404c4d40a4042a545d8b
Rodislav MOLDOVAN <rodislav@gmail.com>
2018-11-25 00:35:20 +0100

    added back hardcoded <a> rel attributes

--- src/app/components/theme/day/Theme.jsx
+++ src/app/components/theme/day/Theme.jsx
@@ -9,36 +9,34 @@ import EmptyProps from "../../../helper/TypeHelper";
 export default class Theme extends React.Component<EmptyProps> {
 
     render() {
-        let rel = "noopener noreferrer nofollow";
-
         return (
             <div className={"theme"}>
 
                 <div className="be_theme-image-left-right"/>
                 <div className="be_theme-image-center"/>
 
-                <a className="be_theme-balloon"  target='_blank' rel={rel}
+                <a className="be_theme-balloon"  target='_blank' rel="noopener noreferrer nofollow"
                    href='http://www.balloon.lu/' title='Click to open Aerostatique Association website'> </a>
 
-                <a className="be_theme-sun" target='_blank' rel={rel}
+                <a className="be_theme-sun" target='_blank' rel="noopener noreferrer nofollow"
                    href='https://www.meteolux.lu/' title='Click to open MeteoLux website'> </a>
 
-                <a className="be_theme-cloud-big" target='_blank' rel={rel}
+                <a className="be_theme-cloud-big" target='_blank' rel="noopener noreferrer nofollow"
                    href='https://www.meteolux.lu/' title='Click to open MeteoLux website'> </a>
 
-                <a className="be_theme-cloud-big-2" target='_blank' rel={rel}
+                <a className="be_theme-cloud-big-2" target='_blank' rel="noopener noreferrer nofollow"
                    href='https://www.meteolux.lu/' title='Click to open MeteoLux website'> </a>
 
-                <a className="be_theme-cloud-medium" target='_blank' rel={rel}
+                <a className="be_theme-cloud-medium" target='_blank' rel="noopener noreferrer nofollow"
                    href='https://www.meteolux.lu/' title='Click to open MeteoLux website'> </a>
 
-                <a className="be_theme-cloud-small" target='_blank' rel={rel}
+                <a className="be_theme-cloud-small" target='_blank' rel="noopener noreferrer nofollow"
                    href='https://www.meteolux.lu/' title='Click to open MeteoLux website'> </a>
 
-                <a className="be_plane" target='_blank' rel={rel}
+                <a className="be_plane" target='_blank' rel="noopener noreferrer nofollow"
                    href='https://www.luxair.lu/fr' title='Click to open LuxAir website'> </a>
 
-                <a className="be_univers" target='_blank' rel={rel}
+                <a className="be_univers" target='_blank' rel="noopener noreferrer nofollow"
                    href='https://spaceresources.public.lu/en.html' title='Click to open Space Resources website'> </a>
 
                 <div className="be_star-small"/>
